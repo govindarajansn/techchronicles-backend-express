3d55237681fdde84a4fc6447b136746d35a2b153
Sneha Govindarajan <govindarajan.sn@northeastern.edu>
2023-10-05 17:54:41 -0400

    move api to vercel.json

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: pg,
+    dialect: "postgres",
     pool: {
       max: 5,
       min: 0,
